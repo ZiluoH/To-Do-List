066d1b0f5284548ab1720de51545a108c972e140
ZiluoH <ziluoh@gmail.com>
2017-09-22 22:04:16 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-*
+$('#todolist ul').sortable({
+	item:"li:not('.listtitle, .additem')",
+	connectWith:"ul",
+	dropOnEmpty:true,
+	placeholder:"emptySpace"
+});
+
+$('input').keydown(function(e){
+	if(e.keyCode == 13){
+		var item = $(this).val();
+
+		$(this).parent().parent().append('<li class=addeditem>' + item + '</li>');
+		$(this).val('');
+	}
+});
+
+$('#trash').droppable({
+	drop: function(event, ui){
+		ui.draggable.remove();
+	}
+})
